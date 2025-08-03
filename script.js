e7e7ddafaf9f4ed8f8daa4017c7b1b648e74df68
Ruona Eruteya <ruonaeruteya@gmail.com>
2025-08-02 18:07:51 -1100

    AOS added

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const menuBtn = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {navLinks.classList.toggle('open');});
+
+AOS.init();
